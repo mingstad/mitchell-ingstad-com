e3db0e982623036a2aecb4841e9a095b791f7050
Mitchell Ingstad <26363842+mingstad@users.noreply.github.com>
2021-04-02 13:31:42 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Mitchell Ingstad',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'This is my personal site. It is primarily used as a sandbox to experiment with web development.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
